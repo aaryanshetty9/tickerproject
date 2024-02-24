b09fceaac48f8a22c1b1bd0e727860ae96560ab6
Vishnu Varadhan <vjvara27@colby.edu>
2024-02-24 03:37:29 -0500

    added sentiment

--- Ticker Project/config.py
+++ Ticker Project/config.py
@@ -1,8 +1,8 @@
-NEWS_API_KEY = 'cfce08b851f74522a919d7ca5e90ca83'
-#COHERE_API_KEY = 'vTR4hQ6fZK9gUF6D9jXulIxbUP56ZV1pQT9y0VYe'
-OPEN_AI_API_KEY = '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'
-username="EyalShechtman"
-password="tickerproject"
-client_id="rchfsvf5HdrCS15nzAgaZQ"
-client_secret="UeQIrIFdKaX-4uXYF27QoxV-sOBLRA"
-user_agent = "tickerproject by /u/EyalShechtman"
+NEWS_API_KEY = ''
+#COHERE_API_KEY = ''
+OPEN_AI_API_KEY = ''
+username=""
+password=""
+client_id=""
+client_secret=""
+user_agent = ""
